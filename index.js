6f036ac68bf17d795ce02a261100ac9282a9c7d4
Yeison <enmanuelquiterios@gmail.com>
2021-08-26 18:30:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employees = {
+    name: 'Yeison', 
+    streetAddress: 'Juan de los palotes'
+}
+
+function updateEmployeeWithKeyAndValue(employee,key, value){
+const newObject = {...employee,[key]:value}
+
+return newObject;
+
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(Object,key,value){
+    Object[key ]= value;
+
+    return Object;
+}
+
+
+
+function deleteFromEmployeeByKey(Object,key){
+    const newObj = [...Object];
+    const cleamVersion = delete newObj[key];
+    return cleamVersion;
+}
+
+function destructivelyDeleteFromEmployeeByKey(Object,key){
+    const clean = delete Object[key];
+    return clean;
+}
